7db42e7f10703ab837fcd412f479bae6d4887057
roconn <roconn25@gmail.com>
2022-05-28 20:15:23 +0100

    renaming classes in menubar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,24 +3,6 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
-// function MenuBar() {
-//   return (
-//     <Navbar bg="light" expand="lg">
-//       <Container>
-//         <Navbar.Brand href="#home">Hello!</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="me-auto">
-//             <Nav.Link href="#home">About</Nav.Link>
-//             <Nav.Link href="#link">Projects</Nav.Link>
-//             <Nav.Link href="#link">CV</Nav.Link>
-//             <Nav.Link href="#link">Contact</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// }
 
 function MenuBar({ currentPage, handlePageChange }) {
   return (
@@ -43,7 +25,7 @@ function MenuBar({ currentPage, handlePageChange }) {
               href="Projects"
               onClick={() => handlePageChange("Projects")}
               className={
-                currentPage === "About" ? "nav-link active" : "nav-link"
+                currentPage === "Projects" ? "nav-link active" : "nav-link"
               }
             >
               Projects
@@ -61,7 +43,7 @@ function MenuBar({ currentPage, handlePageChange }) {
               href="CV"
               onClick={() => handlePageChange("CV")}
               className={
-                currentPage === "About" ? "nav-link active" : "nav-link"
+                currentPage === "CV" ? "nav-link active" : "nav-link"
               }
             >
               CV
