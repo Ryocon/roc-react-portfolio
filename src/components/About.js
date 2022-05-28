072edbb32004d48f4f88b6aa026454da852ded47
roconn <roconn25@gmail.com>
2022-05-28 21:55:30 +0100

    added assets folder for images

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
 import React from "react";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from 'react-bootstrap/Image'
+import heroImg from "../assets/images/rochimselfsmol.png";
 
 function About() {
   return (
- <div>
-   what on earth
- </div>
+    <Container>
+      <Row>
+        <Col>Test</Col>
+        <Col><Image src={heroImg} fluid></Image></Col>
+      </Row>
+    </Container>
   );
 }
 
-export default About
+export default About;
